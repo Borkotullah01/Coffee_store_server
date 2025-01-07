be5f09d8aa92d8e1f3708b848efa82b8405c0693
jawadBashar <ak.borkotullah@gmail.com>
2025-01-07 20:24:44 +0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = process.env.PORT  || 5000;
 require('dotenv').config()
 
 // middleware
@@ -23,6 +23,7 @@ async function run() {
   try {
     const CoffeesDB = client.db("CoffeesDB");
     const CoffeeColl = CoffeesDB.collection("CoffeeColl");
+    const UserColl = CoffeesDB.collection("UserColl");
 
     await client.connect();
     await client.db("admin").command({ ping: 1 });
@@ -72,6 +73,39 @@ async function run() {
         }
       }
       const result = await CoffeeColl.updateOne(filter, newCoffee, options);
+      res.send(result);
+    })
+
+    app.get("/users", async(req, res)=>{
+      const cursor = UserColl.find({});
+      const Users = await cursor.toArray();
+      res.send(Users);
+    })
+    
+    app.post("/users", async(req, res)=>{
+      const user = req.body;
+      const result = await UserColl.insertOne(user);
+      res.send(result);
+    })
+
+    app.delete("/users/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await UserColl.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch("/users", async(req, res)=>{
+      const user = req.body;
+      const email = user.email;
+      const filter = {email}
+      const options = {upsert: true}
+      const newUser = {
+        $set: {
+          lastSignInTime: req.body.lastSignInTime
+        }
+      }
+      const result = await UserColl.updateOne(filter, newUser, options);
       res.send(result);
     })
 
